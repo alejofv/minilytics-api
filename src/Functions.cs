ee262172b211b2be72829ee935072be225252700
Alejandro Figueroa <alexphi@gmail.com>
2020-10-11 16:08:29 -0500

    Scaffolding

---
+++ src/Functions.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using MediatR;
+
+namespace Alejof.Minilytics
+{
+    public class Functions
+    {
+        private readonly IMediator _mediator;
+
+        public Functions(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [FunctionName(nameof(Warmup))]
+        public IActionResult Warmup(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "warmup")]HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation($"C# Http trigger function processed: {nameof(Warmup)}");
+            return new OkResult();
+        }
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,15 @@
+using MediatR;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Alejof.Minilytics.Startup))]
+namespace Alejof.Minilytics
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // builder.Services.AddFunctionSettings();
+            builder.Services.AddMediatR(typeof(Startup));
+        }
+    }
+}
